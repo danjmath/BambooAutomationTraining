36f78b4309c952d2ee985a153696c087b1714a7a
Brooke Rogers <brogers@bamboohr.com>
2025-08-13 14:05:10 -0400

    Adding a test for removing items from cart

---
+++ tests/add-remove-from-cart.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('add-remove-from-cart', async ({ page }) => {
+
+  //Login
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+  //Add Backpack to cart from products page
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+  //Remove Backpack from cart and cofirm action button set pack to "Add to Cart"
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+  await expect(page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')).toBeVisible();
+});
